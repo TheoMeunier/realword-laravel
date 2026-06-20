961aecf9b0a9eb3315aa063e4a035d28e07f7a98
tmeunier <theo.meunier41@gmail.com>
2026-06-20 10:38:54 +0200

    fix clear code

--- public/frankenphp-worker.php
+++ public/frankenphp-worker.php
@@ -1,9 +1,10 @@
 <?php
 
 declare(strict_types=1);
+use Illuminate\Support\Env;
 
 // Set a default for the application base path and public path if they are missing...
-$_SERVER['APP_BASE_PATH'] = \Illuminate\Support\Env::get('APP_BASE_PATH', $_SERVER['APP_BASE_PATH'] ?? __DIR__.'/..');
-$_SERVER['APP_PUBLIC_PATH'] = \Illuminate\Support\Env::get('APP_PUBLIC_PATH', $_SERVER['APP_PUBLIC_PATH'] ?? __DIR__);
+$_SERVER['APP_BASE_PATH'] = Env::get('APP_BASE_PATH', $_SERVER['APP_BASE_PATH'] ?? __DIR__.'/..');
+$_SERVER['APP_PUBLIC_PATH'] = Env::get('APP_PUBLIC_PATH', $_SERVER['APP_PUBLIC_PATH'] ?? __DIR__);
 
 require __DIR__.'/../vendor/laravel/octane/bin/frankenphp-worker.php';
