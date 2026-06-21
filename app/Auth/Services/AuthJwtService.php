1c39bdb9ed540f30ddd21ea7a30b91216cbb4ca8
tmeunier <theo.meunier41@gmail.com>
2026-06-21 12:56:25 +0200

    feat: implement JWT authentication and update environment configuration

--- app/Auth/Services/AuthJwtService.php
+++ app/Auth/Services/AuthJwtService.php
@@ -5,17 +5,27 @@ declare(strict_types=1);
 namespace App\Auth\Services;
 
 use App\Auth\Models\User;
-use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Auth\AuthManager;
+use Tymon\JWTAuth\JWTGuard;
 
-class AuthJwtService
+readonly class AuthJwtService
 {
+    private JWTGuard $guard;
+
+    public function __construct(AuthManager $auth)
+    {
+        /** @var JWTGuard $guard */
+        $guard = $auth->guard('api');
+        $this->guard = $guard;
+    }
+
     public function generateToken(User $user): string
     {
-        return JWTAuth::fromUser($user);
+        return $this->guard->login($user);
     }
 
     public function revokeToken(): void
     {
-        JWTAuth::parseToken()->invalidate();
+        $this->guard->logout();
     }
 }
